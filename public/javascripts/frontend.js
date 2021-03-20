6d6b8469a0629a1cdd43ca2f48e6414ae52b18d2
Liang <goodbye388@gmail.com>
2021-03-20 21:45:41 +0800

    feat: Add default content into edit form

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -4,3 +4,27 @@ function getIdAndSetupRoute(element) {
   const formhook = document.getElementById(`form-${usage}`)
   formhook.action = `/restaurants/${id}/${usage}`
 }
+
+// Get restaurants attributes and send to Edit-form
+function getInfo(element) {
+  setupDefaultInfo(
+    element,
+    'name',
+    'name_en',
+    'category',
+    'phone',
+    'rating',
+    'location',
+    'google_map',
+    'image',
+    'description'
+  )
+  getIdAndSetupRoute(element)
+}
+
+function setupDefaultInfo(element, ...dataAttibuteName) {
+  for (let i = 1; i < arguments.length; i++) {
+    inputhook = document.getElementById(`input-${arguments[i]}`)
+    inputhook.value = element.getAttribute(`data-${arguments[i]}`)
+  }
+}
