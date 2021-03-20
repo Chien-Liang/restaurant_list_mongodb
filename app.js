ab57c35f803f69dde428edc89c2ece2de45eb073
Liang <goodbye388@gmail.com>
2021-03-20 22:07:22 +0800

    feat: Impot body-parser & Implement editRestaurant function

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // Middlewares
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extname: true }))
+app.use(express.json())
 
 // MongoDB Connection
 mongoose.connect('mongodb://localhost/restaurant-list', {
@@ -25,6 +28,7 @@ app.get('/', index.getAllRestaurants)
 app.get('/search', index.searchRestaurants)
 app.get('/restaurants/:id', index.showRestaurant)
 app.post('/restaurants/:id/delete', post.deleteRestaurant)
+app.post('/restaurants/:id/edit', post.editRestaurant)
 
 // Link to server
 app.listen(3000, () => console.log('Listening to http://localhost:3000'))
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -6,3 +6,14 @@ exports.deleteRestaurant = (req, res) => {
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 }
+
+exports.editRestaurant = (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => {
+      Object.assign(restaurant, req.body)
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(err => console.log(err))
+}
